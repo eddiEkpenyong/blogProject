d7b8871b32cf54b77a6082f720b90f9ae6203da9
eddiEkpenyong <edidiong.ekpenyong.19@gmail.com>
2022-07-03 17:57:36 +0100

    Updated Dashboard

--- resources/views/index/dashboard.blade.php
+++ resources/views/index/dashboard.blade.php
@@ -41,17 +41,79 @@
 @endsection
 
 @section('firstDiv')
-<div class="row">
+    <div class="row">
 
-    <div class="col-md-6" style="margin-top: 8rem">
-        <div class="mb-0 h5 d-sm-block">Daily Updates On Cryptocurrencies</div>
-        <div class="h1 mt-0 p-0" style="font-size: 4rem; color: #3f0a8a">EDVIN->BLOG</div>
+        <div class="col-md-6" style="margin-top: 8rem">
+            <div class="mb-0 h5 d-sm-block">Daily Updates On Cryptocurrencies</div>
+            <div class="h1 mt-0 p-0" style="font-size: 4rem; color: #3f0a8a">EDVIN->BLOG</div>
+        </div>
+
+        <div class="col-md-6">
+            <img src={{ asset('storage/images/deveops.svg') }} alt="" class="img-fluid">
+        </div>
+
     </div>
+    <h6 class="mt-5" style="border-bottom:5px solid black"><span
+            class="bg-primary px-2 pt-1 mb-0 text-white">TRENDING</span></h6>
 
-    <div class="col-md-6">
-        <img src= {{ asset('storage/images/deveops.svg') }} alt=""  class="img-fluid">
+    <div class="container mt-3">
+        <div class="row gy-4 gx-2">
+            <div class="card col-md-3" style="width: 10rem;">
+                <img src={{ asset('storage/images/deveops.svg') }} alt="" class="card-image-top">
+                <div class="card-body">
+                    <a href="#" class="card-text text-dark text-decoration-none" id="" >Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</a>
+                </div>
+            </div>
+            <div class="card col-md-3" style="width: 10rem;">
+                <img src={{ asset('storage/images/deveops.svg') }} alt="" class="card-image-top">
+                <div class="card-body">
+                    <a href="#" class="card-text text-dark text-decoration-none" id="" >Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</a>
+                </div>
+            </div>
+            <div class="card col-md-3" style="width: 10rem;">
+                <img src={{ asset('storage/images/deveops.svg') }} alt="" class="card-image-top">
+                <div class="card-body">
+                    <a href="#" class="card-text text-dark text-decoration-none" id="" >Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</a>
+                </div>
+            </div>
+            <div class="card col-md-3" style="width: 10rem;">
+                <img src={{ asset('storage/images/deveops.svg') }} alt="" class="card-image-top">
+                <div class="card-body">
+                    <a href="#" class="card-text text-dark text-decoration-none" id="" >Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</a>
+                </div>
+            </div>
+            <div class="card col-md-3" style="width: 18rem;">
+                <img src={{ asset('storage/images/deveops.svg') }} alt="" class="card-image-top">
+                <div class="card-body">
+                    <a href="#" class="card-text text-dark text-decoration-none" id="" >Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</a>
+                </div>
+            </div>
+            <div class="card col-md-3" style="width: 18rem;">
+                <img src={{ asset('storage/images/deveops.svg') }} alt="" class="card-image-top">
+                <div class="card-body">
+                    <a href="#" class="card-text text-dark text-decoration-none" id="" >Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</a>
+                </div>
+            </div>
+            <div class="card col-md-3" style="width: 18rem;">
+                <img src={{ asset('storage/images/deveops.svg') }} alt="" class="card-image-top">
+                <div class="card-body">
+                    <a href="#" class="card-text text-dark text-decoration-none" id="" >Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</a>
+                </div>
+            </div>
+            <div class="card col-md-3" style="width: 18rem;">
+                <img src={{ asset('storage/images/deveops.svg') }} alt="" class="card-image-top">
+                <div class="card-body">
+                    <a href="#" class="card-text text-dark text-decoration-none" id="" >Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</a>
+                </div>
+            </div>
+        </div>
     </div>
-
-</div>
-<h6 class="mt-5" style="border-bottom:5px solid black"><span class="bg-primary px-2 pt-1 mb-0 text-white">TRENDING</span></h6>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <style>
         *{
